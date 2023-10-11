b69ce15f6a779daa28742dbfcab8e22f0c90bdec
Filipe Vieira, Who? <filipevieirawho@gmail.com>
2023-10-11 03:02:32 -0300

    L6

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 const app = Vue.createApp ({
     data () {
         return {
+            cart: 0,
             product: 'Socks',
-            description: 'This is the best socks'
+            image: './assets/images/socks_green.jpg',
+            inventory: 50,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                { id:2234, color:'green', image: './assets/images/socks_green.jpg' },
+                { id:2235, color:'blue', image: './assets/images/socks_blue.jpg' },
+            ],
         }
 
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        removeFromCart() {
+            this.cart -=1
+        },
+
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
     }
 })
